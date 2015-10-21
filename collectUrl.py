7f2a00fdfbd49f0c498e4ac3540c19d1e6de6b5f
zhangbojian <305151847@qq.com>
2015-10-21 23:47:28 +0800

    Initial commit add py file 整理代码 alter by zhangbojian 增加多线程处理

--- collectUrl.py
+++ collectUrl.py
@@ -6,21 +6,12 @@ import MySQLdb
 import datetime
 import GetInertMessage
 import GetInterfaceData
+import threading
 
 
 
-#接口地址
-url="http://10.245.254.110:8080/etl_platform/rest/service.shtml"
-#接口参数
-values="{\"identify\": \"87da5cdc-6b4a-48f6-8014-470441b66383\", \"userName\":\"ST_BIGDATA\",\"password\":\"Gmcc_345\",\"systemName\":\"STORM\",\"parameter\":{\"rn1\":\"1\",\"rn2\":\"600000\"}}"
-#取接口数据，下载到项目目录
-GetInterfaceData.downloadInterfaceDate(url,values)
 
-#解压数据，获取其中的url
-zp=zipfile.ZipFile('data.zip')
-fileall=zp.open(zp.namelist()[0],pwd='Gmcc_345')
-link=fileall.readline()[:-2]
-while link:
+def collectUrl(link):
     #对URL进行爬虫操作
     try:
         targetPage=ul2.urlopen(link,timeout=120).read()
@@ -50,8 +41,32 @@ while link:
         conn.close()
     except Exception,e:
         print '3',link,e.message
+
+
+if "__name__"=="__main__":
+    #接口地址
+    url="http://10.245.254.110:8080/etl_platform/rest/service.shtml"
+    #接口参数
+    values="{\"identify\": \"87da5cdc-6b4a-48f6-8014-470441b66383\", \"userName\":\"ST_BIGDATA\",\"password\":\"Gmcc_345\",\"systemName\":\"STORM\",\"parameter\":{\"rn1\":\"1\",\"rn2\":\"600000\"}}"
+    #取接口数据，下载到项目目录
+    GetInterfaceData.downloadInterfaceDate(url,values)
+
+    #解压数据，获取其中的url
+    zp=zipfile.ZipFile('data.zip')
+    fileall=zp.open(zp.namelist()[0],pwd='Gmcc_345')
     link=fileall.readline()[:-2]
-fileall.close()
+    while link:
+        ts=[]
+        i=0
+        while i<10 and link:
+            tn=threading.Thread(target=collectUrl,args=(link,))
+            ts.append(tn)
+            link=fileall.readline()[:-2]
+        for t in ts:
+            t.setDaemon(True)
+            t.start()
+        t.join()
+    fileall.close()
 
 
 
